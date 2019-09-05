d838a592041c21a9c4c19d94a097d1f42ca2ebd0
Khye Ean Low <lowkhyeean@gmail.com>
2019-09-05 18:55:33 +1000

    refactor

--- src/main/java/net/digibi/mailerlite/MailerLiteClient.java
+++ src/main/java/net/digibi/mailerlite/MailerLiteClient.java
@@ -1,8 +1,11 @@
 package net.digibi.mailerlite;
 
+import java.util.Map;
+
 import org.json.JSONObject;
 
-import kong.unirest.*;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
 /**
  * MailerLiteClient
@@ -36,105 +39,65 @@ public class MailerLiteClient {
      * Moves user(identified by email, name) to specified groupId and set type of subscription
      * @param groupId
      * @param email
-     * @param name
      * @param type allowed values: unsubscribed, active, unconfirmed
      * @return
      */
-    public boolean moveSubscriberToGroup(long groupId, String email, String name, String type) {
-        return moveSubscriberToGroup(groupId, createUserBodyToSend(email, name, null, type));
+    public boolean moveSubscriberToGroup(long groupId, String email, String type) {
+        return moveSubscriberToGroup(groupId, createUserBodyToSend(email, null, type));
     }
 
     /**
      * Moves user(identified by email, name) to specified groupId, set fields & type of subscription
      * @param groupId
      * @param email
-     * @param name
      * @param fields fields to set
      * @param type allowed values: unsubscribed, active, unconfirmed
      * @return
      */
-    public boolean moveSubscriberToGroup(long groupId, String email, String name, String fields, String type) {
-        return moveSubscriberToGroup(groupId, createUserBodyToSend(email, name, fields, type));
+    public boolean moveSubscriberToGroup(long groupId, String email, Map<String, String> fields, String type) {
+        return moveSubscriberToGroup(groupId, createUserBodyToSend(email, fields, type));
     }
 
     /**
      * Method to construct String in JSON format
      * 
      * @param email
-     * @param name
      * @param fields
      * @param type   values accepted: unsubscribed, active
      * @return String in JSON format
      */
-    public static String createUserBodyToSend(String email, String name, String fields, String type) {
-        JSONObject jsonObject = new JSONObject().put("email", email).put("name", name).put("type", type);
+    public static String createUserBodyToSend(String email, Map<String, String> fields, String type) {
+        JSONObject jsonObject = new JSONObject().put("email", email).put("type", type);
         if (fields != null) {
-            jsonObject.put("fields", fields);
+            jsonObject.put("fields", (new JSONObject(fields)).toString());
         }
         return jsonObject.toString();
     }
+    
+    public HttpResponse<String> updateUserDetails(int identifier, Map<String, String> fields) {
+        HttpResponse<String> response = Unirest
+            .put(API_END_POINT + "subscribers/" + identifier)
+            .header("content-type", "application/json")
+            .header("x-mailerlite-apikey", API_KEY)
+            .body((new JSONObject(fields)).toString())
+            .asString();
+        return response;
+    }
+    
+    public HttpResponse<String> updateUserDetails(String identifier, Map<String, String> fields) {
+        HttpResponse<String> response = Unirest
+            .put(API_END_POINT + "subscribers/" + identifier)
+            .header("content-type", "application/json")
+            .header("x-mailerlite-apikey", API_KEY)
+            .body((new JSONObject(fields)).toString())
+            .asString();
+        return response;
+    }
 
-    
-    /**
-     * Method to update the details of a user
-     * 
-     * @param identifier User ID
-     * @param updateBody String in JSON format with the key-pair(s) to update
-     * @return Updated user details in JSON format
-     */
-    public HttpResponse<String> updateUserDetails(int identifier, String updateBody) {
-        HttpResponse<String> response = Unirest.put(API_END_POINT + "subscribers/" + identifier).header("content-type", "application/json").header("x-mailerlite-apikey", API_KEY).body(updateBody)
-        .asString();
-        return response;
-    }
-    
-    /**
-     * Method to update the details of a user
-     * @param identifier User ID
-     * @param name  Name of User
-     * @param fields Fields to update
-     * @return
-     */
-    public HttpResponse<String> updateUserDetails(int identifier, String name, String fields) {
-        return updateUserDetails(identifier, createUpdateBodyToSend(name, fields));
-    }
-    
-    
-    /**
-     * Method to update the details of a user
-     * 
-     * @param identifier User Email
-     * @param updateBody String in JSON format with the key-pair(s) to update
-     * @return Updated user details in JSON format
-     */
-    public HttpResponse<String> updateUserDetails(String identifier, String updateBody) {
-        HttpResponse<String> response = Unirest.put(API_END_POINT + "subscribers/" + identifier).header("content-type", "application/json").header("x-mailerlite-apikey", API_KEY).body(updateBody)
-        .asString();
-        return response;
-    }
-    
-    /**
-     * Method to update the details of a user
-     * @param identifier User Email
-     * @param name  Name of User
-     * @param fields Fields to update
-     * @return
-     */
-    public HttpResponse<String> updateUserDetails(String identifier, String name, String fields) {
-        return updateUserDetails(identifier, createUpdateBodyToSend(name, fields));
-    }
-    
-    /**
-     * Method to construct String in JSON format
-     * @param name User's name
-     * @param fields Fields to update
-     * @return
-     */
-    public static String createUpdateBodyToSend(String name, String fields) {
-        JSONObject fieldParsed = new JSONObject(fields);
+    public static String createUpdateBodyToSend(String name, Map<String, String> fields) {
         JSONObject jsonObject = new JSONObject().put("name", name);
         if (fields != null) {
-            jsonObject.put("fields", fieldParsed);
+            jsonObject.put("fields", (new JSONObject(fields)).toString());
         }
         return jsonObject.toString();
     }
